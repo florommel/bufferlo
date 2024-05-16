44f0ca8ef39f201ec929bd68462cb66ff6b442b4
Florian Rommel <mail@florommel.de>
2024-05-16 18:24:26 +0200

    Version 0.8

--- bufferlo.el
+++ bufferlo.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Florian Rommel <mail@florommel.de>
 ;; Url: https://github.com/florommel/bufferlo
 ;; Created: 2021-09-15
-;; Version: 0.7
+;; Version: 0.8
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: buffer frame tabs local
 
