8fc587ba341b2ec8189b4d948accc90140469147
Florian Rommel <mail@florommel.de>
2025-09-14 20:43:05 +0200

    Version 1.2

--- bufferlo.el
+++ bufferlo.el
@@ -8,7 +8,7 @@
 ;;             Stephane Marks <shipmints@gmail.com>
 ;; Url: https://github.com/florommel/bufferlo
 ;; Created: 2021-09-15
-;; Version: 1.1
+;; Version: 1.2
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: buffer frame tabs local
 
